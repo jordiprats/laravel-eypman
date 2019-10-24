849d51349c973d079f5d54acbd7dd24a1fba5406
jordiprats <jordi.prats@gmail.com>
2019-10-24 22:42:45 +0200

    preofile button

--- eypman/resources/views/layouts/app.blade.php
+++ eypman/resources/views/layouts/app.blade.php
@@ -61,12 +61,19 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('home') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('home-form').submit();">
+                                        {{ __('Profile') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
+                                    <form id="home-form" action="{{ route('home') }}" method="GET" style="display: none;">
+                                    </form>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
