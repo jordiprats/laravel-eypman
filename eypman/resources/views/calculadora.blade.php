a68bd771d8604a6514990300f965cedc2742d25f
Jordi Prats <jordi.prats@gmail.com>
2019-12-12 09:40:56 +0100

    canvi titol

--- eypman/resources/views/calculadora.blade.php
+++ eypman/resources/views/calculadora.blade.php
@@ -6,7 +6,7 @@
       <div class="row justify-content-center">
           <div class="col-md-8">
               <div class="card">
-                  <div class="card-header">calculadora preus</div>
+                  <div class="card-header">calculadora extres</div>
 
                   <div class="card-body">
                       <center><img src="/img/money_paid_loop.gif" /></center>
